3cdf66a470db3fba4ebb0a8124e24b42f568eed4
mauricionr <mauricioskateboard@gmail.com>
2017-07-15 17:26:32 -0300

    added dynamic cors for amp

--- index.js
+++ index.js
@@ -10,153 +10,161 @@ app.use(cookieParser())
 // GTM passthrough example
 app.get('/gtm-analytics.config.json', (req, res) => {
 
-  const domain = req.headers.host.split(':')[0]
-  const gaCookie = req.cookies._ga || generateGaCookie(domain)
-  const clientId = parseClientIdFromGaCookie(gaCookie)
-  const cookieString = generateCookieString({
-    name: '_ga',
-    value: gaCookie,
-    domain: domain.replace('www.', ''),
-    path: '/',
-    expires: new Date(1000 * 60 * 60 * 24 * 365 * 2 + (+new Date)).toGMTString()
-  })
-  const stockCidRegex = /CLIENT_ID\([^)]*\)/g
+    const domain = req.headers.host.split(':')[0]
+    const referer = getReferer(request);
+    const gaCookie = req.cookies._ga || generateGaCookie(domain)
+    const clientId = parseClientIdFromGaCookie(gaCookie)
+    const cookieString = generateCookieString({
+        name: '_ga',
+        value: gaCookie,
+        domain: domain.replace('www.', ''),
+        path: '/',
+        expires: new Date(1000 * 60 * 60 * 24 * 365 * 2 + (+new Date)).toGMTString()
+    })
+    const stockCidRegex = /CLIENT_ID\([^)]*\)/g
 
-  res.setHeader('Set-Cookie', cookieString)
-  // You may wish to set this dynamically - only one is allowed and it cannot
-  // be a wildcard '*' when using Allow-Credentials
-  res.setHeader('Access-Control-Allow-Origin', 'https://cdn.ampproject.org')
-  res.setHeader('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin')
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-  // AMP-specific header, check your protocol
-  res.setHeader('AMP-Access-Control-Allow-Source-Origin', 'https://' + domain)
-
-  /**
-   * This approach takes advantage of Google Tag Manager AMP Containers
-   * https://analytics.googleblog.com/2016/10/google-tag-manager-giving-mobile.html
-   * GTM returns properly formatted JSON for tags added in the interface, and
-   * we extend the JSON with our Client ID here. Using GTM with AMP is recommended
-   */
-  request.get({
-    url: 'https://www.googletagmanager.com/amp.json',
-    qs: req.query,
-    json: true
-  }, (err, response, data) => {
-
-    if (err) data = {"vars": {}}  // Add additional error handling here
+    res.setHeader('Set-Cookie', cookieString)
+        // cannot be a wildcard '*' when using Allow-Credentials
+    res.setHeader('Access-Control-Allow-Origin', referer)
+    res.setHeader('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+        // AMP-specific header, check your protocol
+    res.setHeader('AMP-Access-Control-Allow-Source-Origin', 'https://' + domain)
 
     /**
-     * On a side note, we're only adding the client ID value here, but
-     * you could add other dynamic data, too, such as a User ID.
-     * Just be aware that you generally have to manually insert the new data into
-     * the 'responses' properties returned by GTM, which can be a bit of a pain.
+     * This approach takes advantage of Google Tag Manager AMP Containers
+     * https://analytics.googleblog.com/2016/10/google-tag-manager-giving-mobile.html
+     * GTM returns properly formatted JSON for tags added in the interface, and
+     * we extend the JSON with our Client ID here. Using GTM with AMP is recommended
      */
-    data.vars.clientId = clientId
+    request.get({
+        url: 'https://www.googletagmanager.com/amp.json',
+        qs: req.query,
+        json: true
+    }, (err, response, data) => {
 
-    data.requests = Object.keys(data.requests)
-      .reduce((map, key) => {
+        if (err) data = { "vars": {} } // Add additional error handling here
 
-        map[key] = data.requests[key].replace(stockCidRegex, '${clientId}')
-        return map
+        /**
+         * On a side note, we're only adding the client ID value here, but
+         * you could add other dynamic data, too, such as a User ID.
+         * Just be aware that you generally have to manually insert the new data into
+         * the 'responses' properties returned by GTM, which can be a bit of a pain.
+         */
+        data.vars.clientId = clientId
 
-      }, {})
+        data.requests = Object.keys(data.requests)
+            .reduce((map, key) => {
 
-    res.json(data)
+                map[key] = data.requests[key].replace(stockCidRegex, '${clientId}')
+                return map
 
-  })
+            }, {})
+
+        res.json(data)
+
+    })
 
 })
 
 // Non-GTM example
 app.get('/analytics.config.json', (req, res) => {
 
-  const domain = req.headers.host.split(':')[0]
-  const gaCookie = req.cookies._ga || generateGaCookie(domain)
-  const clientId = parseClientIdFromGaCookie(gaCookie)
-  const cookieString = generateCookieString({
-    name: '_ga',
-    value: gaCookie,
-    domain: domain.replace('www.', ''),
-    path: '/',
-    expires: new Date(1000 * 60 * 60 * 24 * 365 * 2 + (+new Date)).toGMTString()
-  })
+    const domain = req.headers.host.split(':')[0]
+    const gaCookie = req.cookies._ga || generateGaCookie(domain)
+    const clientId = parseClientIdFromGaCookie(gaCookie)
+    const cookieString = generateCookieString({
+        name: '_ga',
+        value: gaCookie,
+        domain: domain.replace('www.', ''),
+        path: '/',
+        expires: new Date(1000 * 60 * 60 * 24 * 365 * 2 + (+new Date)).toGMTString()
+    })
 
-  res.setHeader('Set-Cookie', cookieString)
-  // You may wish to set this dynamically - only one is allowed and it cannot
-  // be a wildcard '*' when using Allow-Credentials
-  res.setHeader('Access-Control-Allow-Origin', 'https://cdn.ampproject.org')
-  res.setHeader('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin')
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-  // AMP-specific header
-  res.setHeader('AMP-Access-Control-Allow-Source-Origin', 'http://' + domain)
+    res.setHeader('Set-Cookie', cookieString)
+        // You may wish to set this dynamically - only one is allowed and it cannot
+        // be a wildcard '*' when using Allow-Credentials
+    res.setHeader('Access-Control-Allow-Origin', 'https://cdn.ampproject.org')
+    res.setHeader('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+        // AMP-specific header
+    res.setHeader('AMP-Access-Control-Allow-Source-Origin', 'http://' + domain)
 
-  /**
-   * This counts on extending the default Google Analytics component
-   * (<amp-analytics type="googleanalytics"></amp-analytics>)
-   * If you'd like to roll your own configuration instead of using the
-   * prepared defaults, do this here as well. For more, see:
-   * https://www.ampproject.org/docs/reference/components/amp-analytics#configuration
-   *
-   * You can set additional variables here, too - see the note in the GTM example
-   * above for some ideas.
-   */
-  res.json({
-    "vars": {
-      "clientId": clientId
-    }
-  })
+    /**
+     * This counts on extending the default Google Analytics component
+     * (<amp-analytics type="googleanalytics"></amp-analytics>)
+     * If you'd like to roll your own configuration instead of using the
+     * prepared defaults, do this here as well. For more, see:
+     * https://www.ampproject.org/docs/reference/components/amp-analytics#configuration
+     *
+     * You can set additional variables here, too - see the note in the GTM example
+     * above for some ideas.
+     */
+    res.json({
+        "vars": {
+            "clientId": clientId
+        }
+    })
 
 })
 
 app.listen(port, (err) => {
 
-  if (err) return console.log(err)
+    if (err) return console.log(err)
 
-  console.log('Listening on port ' + port)
+    console.log('Listening on port ' + port)
 
 })
 
 function parseClientIdFromGaCookie(gaCookie) {
 
-  return gaCookie.split('.').slice(-2).join('.')
+    return gaCookie.split('.').slice(-2).join('.')
 
 }
 
 function generateGaCookie(host) {
 
-  return [
-    'GA1',
-     host.replace('www.', '').split('.').length,
-     generateGaClientId()
-  ].join('.')
+    return [
+        'GA1',
+        host.replace('www.', '').split('.').length,
+        generateGaClientId()
+    ].join('.')
 
 }
 
 function generateGaClientId() {
 
-  const rand = Math.round(Math.random() * 2147483647)
-  const ts = Math.round(+new Date() / 1000.0)
+    const rand = Math.round(Math.random() * 2147483647)
+    const ts = Math.round(+new Date() / 1000.0)
 
-  return [rand, ts].join('.')
+    return [rand, ts].join('.')
 
 }
 
 function generateCookieString(config) {
 
-  let base = [[config.name, config.value]];
+    let base = [
+        [config.name, config.value]
+    ];
 
-  ['domain', 'path', 'expires'].forEach(opt => {
+    ['domain', 'path', 'expires'].forEach(opt => {
 
-    if (isDefined_(config[opt])) base.push([opt, config[opt]])
+        if (isDefined_(config[opt])) base.push([opt, config[opt]])
 
-  })
+    })
 
-  return base.map(pair => pair.join('=') + ';').join('')
+    return base.map(pair => pair.join('=') + ';').join('')
 
 }
 
 function isDefined_(thing) {
 
-  return typeof thing !== 'undefined'
+    return typeof thing !== 'undefined'
 
+}
+
+function getReferer(request) {
+    let urlExtractor = /(https?:\/\/[^\/]+)\/?.*$/i;
+    let refererMatch = request.headers.referer.match(urlExtractor);
+    return refererMatch[refererMatch.length - 1];
 }
